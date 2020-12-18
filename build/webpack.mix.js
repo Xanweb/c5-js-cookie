687ef1b220354fb98c2db9597ce63dde3eaa1150
Hamed Darragi <hd@xanweb.com>
2020-12-18 19:14:34 +0100

    Refactor library structure

--- build/webpack.mix.js
+++ build/webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.copy('node_modules/js-cookie/dist/js.cookie.min.js', 'js/js.cookie.min.js');
+mix.copy('node_modules/js-cookie/dist/js.cookie.min.js', '../js.cookie.js');
 
 // Disable mix-manifest.json
 // @see https://github.com/JeffreyWay/laravel-mix/issues/580
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use Xanweb\ExtAsset\Asset\VendorAssetManager;
 VendorAssetManager::registerMultiple([
     'js-cookie' => [
         [
-            'vendor-javascript', 'js/js.cookie.min.js', 'xanweb/c5-js-cookie', [
+            'vendor-javascript', 'js.cookie.js', 'xanweb/c5-js-cookie', [
                 'minify' => false,
                 'version' => '3.0.0-rc.1',
                 'position' => Asset::ASSET_POSITION_HEADER,
