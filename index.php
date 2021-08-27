b9ef2bef84f2430f1969739f3c3cfa4986c0a2ea
Hamed Darragi <hd@xanweb.com>
2021-08-27 18:50:57 +0100

    update to v3.0

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
 
-use Concrete\Core\Asset\Asset;
 use Xanweb\ExtAsset\Asset\VendorAssetManager;
 
-VendorAssetManager::registerMultiple([
-    'js-cookie' => [
-        [
-            'vendor-javascript', 'js.cookie.js', 'xanweb/c5-js-cookie', [
-                'minify' => false,
-                'version' => '3.0.0-rc.1',
-                'position' => Asset::ASSET_POSITION_HEADER,
-            ],
-        ],
-    ],
-]);
+VendorAssetManager::register(
+    'vendor-javascript',
+    'js-cookie',
+    'js.cookie.js',
+    'xanweb/c5-js-cookie',
+    [
+        'minify' => false,
+        'version' => '3.0.0',
+    ]
+);
